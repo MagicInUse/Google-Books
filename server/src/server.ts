156bb51b31fe3222df13dff0ce96608940e28a34
MagicInUse <jacob.norr@live.com>
2024-12-22 15:33:50 -0700

    render deploy fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -26,10 +26,10 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
 db.once('open', () => {
