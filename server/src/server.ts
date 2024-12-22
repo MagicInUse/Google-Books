c3e598367296e449c5a5a6eddea6b85489b87e42
MagicInUse <jacob.norr@live.com>
2024-12-22 15:30:18 -0700

    set the static, forgot to set the get wildcard

--- server/src/server.ts
+++ server/src/server.ts
@@ -28,6 +28,10 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
 db.once('open', () => {
   console.log(`MongoDB connected to: ${db.host}`);
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
