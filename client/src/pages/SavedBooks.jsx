24888b510eeb02827780f28ce24e1ba02c88aa46
MagicInUse <jacob.norr@live.com>
2025-01-07 12:19:59 -0700

    small bug fix. app is much better now!

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -26,9 +26,9 @@ const SavedBooks = () => {
 
   return (
     <>
-      <div fluid className="text-light bg-dark p-5">
-        <Container>
-          <h1>Viewing saved books!</h1>
+      <div className="text-light bg-dark p-5">
+        <Container fluid>
+          <h1>Viewing {userData.username}'s saved books!</h1>
         </Container>
       </div>
       <Container>
