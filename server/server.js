28c14ff95d59e5c6ee2807f67d1b83307320bc34
MagicInUse <jacob.norr@live.com>
2025-01-07 12:49:22 -0700

    render build package changes

--- server/server.js
+++ server/server.js
@@ -29,10 +29,10 @@ const startServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../../client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
 
